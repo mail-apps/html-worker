a2bcac766a4a0c7427dc3fa58e65cc1220e6c7ad
Yonggang Luo <luoyonggang@gmail.com>
2015-09-09 13:19:53 +0800

    Ignore files.

--- install.bat
+++ install.bat
@@ -1,1 +1,21 @@
-npm install --msvs_version=2013
+call npm config set prefix "%AppData%\npm"
+
+call :checkExist npm
+call :checkExist bower
+call :checkExist bower-installer
+call :checkExist bower-update
+call :checkExist npm-check-updates
+cmd /k
+
+goto :eof
+
+:checkExist
+echo Checking for %~1
+where %~1 2>nul 1>nul
+if "%errorlevel%" neq "0" (
+  echo Installing %~1
+  call npm install -g %~1
+) else (
+  echo %~1 already installed.
+)
+goto :eof
